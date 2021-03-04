784ef0d045e07070091a8fdbe3abe163d1a4a58c
Pongthep Vijite <mr.pongthep@gmail.com>
2021-03-04 10:26:24 +0700

    release version 0.0.4

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import Extension, find_packages
 setup(
     name='atlas-metadata-extractor',
     packages=find_packages(),
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     description='This project is used for extract metadata from data source',
     # Give a short description about your library
     author='pongthepv',
     author_email='mr.pongthep@gmail.com',
     url='https://github.com/pongthep/atlas-metadata-extractor',
-    download_url='https://github.com/pongthep/atlas-metadata-extractor/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/pongthep/atlas-metadata-extractor/archive/v0.0.4.tar.gz',
     # I explain this later on
     keywords=['apache atlas', 'atlas', 'extractor'],
     install_requires=[
